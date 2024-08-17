df38434981c2cd01ee8f18877a2d028996f9d800
bagasly <bagasly33@gmail.com>
2024-08-17 14:55:17 +0700

    fix home

--- resources/views/components/home-layout.blade.php
+++ resources/views/components/home-layout.blade.php
@@ -1,42 +1,16 @@
 <!DOCTYPE html>
 <html lang="en">
-<<<<<<< HEAD
-=======
-  <head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <title>Threesixty</title>
-    <link
-      href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
-      rel="stylesheet"
-    />
-    <link 
-      rel="stylesheet" 
-      href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
-    <link
-      rel="stylesheet"
-      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css"
-    />
-    <link href="{{asset('assets/css/style.css')}}" rel="stylesheet" />
-  </head>
-  <body>
-    <header>
-      <x-navbar></x-navbar>
-      <div class="container-fluid banner">
-        <div class="container text-center">
-          <h3 class="display-1 fw-semibold">
-            YOU DREAM IT <br />
-            WE PRINT IT
-          </h3>
->>>>>>> cba1f9c17a342638cc9ef1d352c0f6c825088f44
 
 <head>
   <meta charset="utf-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1" />
   <title>Threesixty</title>
   <link
-    href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
+    href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
     rel="stylesheet" />
+  <link
+    rel="stylesheet"
+    href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
   <link
     rel="stylesheet"
     href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" />
@@ -234,88 +208,13 @@
     </div>
 
     <!-- Service -->
-    <div id="service" class="container-fluid text-center mt-5 pb-5">
-      <h1>OUR SERVICE</h1>
-      <div class="container text-center mt-4">
-        <div class="row justify-content-center gap-5">
-          <div class="col-lg-2 col-md-4 col-sm-6 p-4 shadow-lg bg-body">
-            <div class="row">
-              <div class="col mb-4">
-                <i
-                  class="fa-solid fa-lightbulb img-fluid"
-                  style="color: #000; font-size: 8vw"></i>
-              </div>
-            </div>
-            <div class="row">
-              <div class="col d-grid">
-                <p class="h4">We prioritize</p>
-              </div>
-            </div>
-          </div>
-          <div class="col-lg-2 col-md-4 col-sm-6 p-4 shadow-lg bg-body">
-            <div class="row">
-              <div class="col mb-4">
-                <i
-                  class="fa-solid fa-shirt img-fluid"
-                  style="color: #000; font-size: 8vw"></i>
-              </div>
-            </div>
-            <div class="row">
-              <div for="neutral" class="col d-grid">
-                <p class="h4">We prioritize</p>
-              </div>
-            </div>
-          </div>
-          <div class="col-lg-2 col-md-4 col-sm-6 p-4 shadow-lg bg-body">
-            <div class="row">
-              <div class="col mb-4">
-                <i
-                  class="fa-solid fa-print img-fluid"
-                  style="color: #000; font-size: 8vw"></i>
-              </div>
-            </div>
-            <div class="row">
-              <div for="sad" class="col d-grid">
-                <p class="h4">We prioritize</p>
-              </div>
-            </div>
-          </div>
-          <div class="col-lg-2 col-md-4 col-sm-6 p-4 shadow-lg bg-body">
-            <div class="row">
-              <div class="col mb-4">
-                <i
-                  class="fa-solid fa-truck img-fluid"
-                  style="color: #000; font-size: 8vw"></i>
-              </div>
-            </div>
-            <div class="row">
-              <div for="sad" class="col d-grid">
-                <p class="h4">We prioritize</p>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+    <x-container></x-container>
   </main>
 
   <x-footer></x-footer>
 
   <script src="{{asset('assets/js/global.js')}}"></script>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 
-<<<<<<< HEAD
 </html>
-=======
-      <!-- Service -->
-      <x-container></x-container>
-    </main>
-
-    <x-footer></x-footer>
-
-    <script src="{{asset('assets/js/global.js')}}"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
-  </body>
-</html>
->>>>>>> cba1f9c17a342638cc9ef1d352c0f6c825088f44
