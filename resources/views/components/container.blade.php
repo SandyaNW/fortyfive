6b44710288ff6af29a198f1c34e98137b5aaa081
dimasanu <daji5095@gmail.com>
2024-08-17 14:41:55 +0700

    .

---
+++ resources/views/components/container.blade.php
@@ -0,0 +1,55 @@
+<div id="service" class="container-fluid text-center mt-5 pb-5">
+    <h1>OUR SERVICE</h1>
+    <div class="container text-center mt-4">
+        <div class="row justify-content-center gap-5">
+            <div class="col-lg-2 col-md-4 col-sm-6 p-4 shadow-lg bg-body">
+                <div class="row">
+                    <div class="col mb-4">
+                        <i class="fa-solid fa-lightbulb img-fluid" style="color: #000; font-size: 8vw"></i>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col d-grid">
+                        <p class="h4">We prioritize</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-2 col-md-4 col-sm-6 p-4 shadow-lg bg-body">
+                <div class="row">
+                    <div class="col mb-4">
+                        <i class="fa-solid fa-shirt img-fluid" style="color: #000; font-size: 8vw"></i>
+                    </div>
+                </div>
+                <div class="row">
+                    <div for="neutral" class="col d-grid">
+                        <p class="h4">We prioritize</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-2 col-md-4 col-sm-6 p-4 shadow-lg bg-body">
+                <div class="row">
+                    <div class="col mb-4">
+                        <i class="fa-solid fa-print img-fluid" style="color: #000; font-size: 8vw"></i>
+                    </div>
+                </div>
+                <div class="row">
+                    <div for="sad" class="col d-grid">
+                        <p class="h4">We prioritize</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-2 col-md-4 col-sm-6 p-4 shadow-lg bg-body">
+                <div class="row">
+                    <div class="col mb-4">
+                        <i class="fa-solid fa-truck img-fluid" style="color: #000; font-size: 8vw"></i>
+                    </div>
+                </div>
+                <div class="row">
+                    <div for="sad" class="col d-grid">
+                        <p class="h4">We prioritize</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
